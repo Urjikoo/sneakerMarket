18b0df1112a7d439fd90bed52bd818198dac8c1d
Resilient Coder <resilientcoders@Resilients-MBP.cable.rcn.com>
2021-11-20 00:43:38 -0500

    sneakerapp

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+  url: "mongodb+srv://UrjiH:getpassword@cluster0.rh8ci.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  dbName: "SneakerApp",
+};
